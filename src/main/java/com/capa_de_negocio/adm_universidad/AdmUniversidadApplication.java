f3a32dcca8eaff80511dfaeb9137beb2833d0f93
DwaRD13 <munozdarwin013@gmail.com>
2026-08-07 16:24:40 -0400

    Iniciar proyecto del backend

---
+++ src/main/java/com/capa_de_negocio/adm_universidad/AdmUniversidadApplication.java
@@ -0,0 +1,13 @@
+package com.capa_de_negocio.adm_universidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmUniversidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmUniversidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capa_de_negocio/adm_universidad/AdmUniversidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capa_de_negocio.adm_universidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmUniversidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
