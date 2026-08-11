624eb32447d6b8f0f338a39c3cbf434617fcae51
DwaRD13 <munozdarwin013@gmail.com>
2026-08-11 13:57:47 -0400

    Arreglar los controladores

--- src/main/java/com/capa_de_negocio/adm_universidad/controlador/ControladorDashboardAdmin.java
+++ src/main/java/com/capa_de_negocio/adm_universidad/controlador/ControladorDashboardAdmin.java
@@ -3,10 +3,16 @@ package com.capa_de_negocio.adm_universidad.controlador;
 import com.capa_de_negocio.adm_universidad.servicio.ServicioDashboardAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RestController
+@RequestMapping("/api/admin/dashboard")
+@PreAuthorize("hasRole('Administrativo')")
 public class ControladorDashboardAdmin {
 
     @Autowired
